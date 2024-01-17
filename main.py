a78a4036011cc44deea2055eb48938e5ee39372c
zhs <2067144018@qq.com>
2024-01-17 23:53:44 +0800

    4

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from array2d import array2d
-from Architect2 import SimpleShapes
-
+from Architect2 import SimpleShapes, Rooms
+import test_tools 
 
 grid = array2d(20, 10, 0)
 
 SimpleShapes.draw_rectangle(grid, 1, 5, 5, 2, 3)
 
-grid.draw(2)
+test_tools.print_grid(grid)
 
 grid = array2d(20, 10, 0)
 
 SimpleShapes.draw_circle(grid, 1, 5, 5, 2)
 
-grid.draw(2)
+test_tools.print_grid(grid)
+
+grid = array2d(20, 10, 0)
+
+Rooms.brogue_designCircularRoom(grid)
+
+test_tools.print_grid(grid)
---
+++ test_tools.py
@@ -0,0 +1,29 @@
+
+# 测试用工具
+
+from array2d import array2d
+
+
+def print_grid(grid, symbols:str = '.+#'):
+        symbols = list(symbols)
+        frequency_dict = {}
+        
+        for x in range(grid.width):
+            for y in range(grid.height):
+                element = grid[x,y]
+                if element in frequency_dict:
+                    frequency_dict[element] += 1
+                else:
+                    frequency_dict[element] = 1
+        
+        sorted_elements = sorted(frequency_dict, key=lambda x: frequency_dict[x], reverse=True)
+        symbol_tuples = [(sorted_elements[i] if i < len(sorted_elements) else None, symbols[i] if i < len(symbols) else sorted_elements[i]) for i in range(max(len(sorted_elements), len(symbols)))]
+
+
+        symbol_dict = {}  # {element: symbol}
+        for key, value in symbol_tuples:
+            symbol_dict[key] = value
+        def replace_element_with_symbol(_, __, element):
+            return symbol_dict[element]
+        
+        grid.copy().map2d(replace_element_with_symbol).draw(width=2)
