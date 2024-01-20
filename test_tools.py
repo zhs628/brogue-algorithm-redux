f235a84473531116d32495499bd1ed3dd7acba45
blueloveTH <blueloveTH@foxmail.com>
2024-01-20 21:00:08 +0800

    更新array2d

--- test_tools.py
+++ test_tools.py
@@ -25,7 +25,7 @@ def _random_selection(original_list, ratio):
     return list(selected_elements)
 
 
-def print_grid(grid, symbols: str = ".+#", message="------------------------"):
+def print_grid(grid: array2d, symbols: str = ".+#", message="------------------------"):
     symbols = list(symbols)
     frequency_dict = {}
 
@@ -52,11 +52,8 @@ def print_grid(grid, symbols: str = ".+#", message="------------------------"):
     for key, value in symbol_tuples:
         symbol_dict[key] = value
 
-    def replace_element_with_symbol(_, __, element):
-        return symbol_dict[element]
-
     print(message)
-    grid.copy().map2d(replace_element_with_symbol).draw(width=2)
+    grid.map(symbol_dict.__getitem__).draw(width=2)
 
 
 def test_all_rooms(
